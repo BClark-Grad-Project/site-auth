b62629709b6841d185e3a47cfc143174b933dee4
Brandon L. Clark <support@themindspot.com>
2015-02-16 00:10:17 -0800

    cont test 2 bug fix

--- permissions.js
+++ permissions.js
@@ -8,37 +8,40 @@ var R = require('./read');
 var backURL;
 
 module.exports.registered = function(req, res, next){
+	backURL=req.header('Referer') || '/';
 	R.get({_id:req.session.user.id}, function(err, user){
 		if(err){res.redirect(backURL);}
 		if(!user){
-			return false;
+			return res.redirect(backURL);
 		}	else {
-			next();
+			return next();
 		}
 	});	
 };
 
 module.exports.admin = function(req, res, next){
+	backURL=req.header('Referer') || '/';
 	R.get({_id:req.session.user.id}, function(err, user){
 		if(err){res.redirect(backURL);}
 		if(user){
 			if(user.credentials.type !== 'admin'){
-				return false;
+				return res.redirect(backURL);
 			}	else {
-				next();
+				return next();
 			}
 		}	
 	});
 };
 
 module.exports.general = function(req, res, next){
+	backURL=req.header('Referer') || '/';
 	R.get({_id:req.session.user.id}, function(err, user){
 		if(err){res.redirect(backURL);}
 		if(user){
 			if(user.credentials.type !== 'general'){
-				return false;
+				return res.redirect(backURL);
 			} else {
-				next();
+				return next();
 			}
 		} 
 	});
