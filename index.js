5437cae277da2b452b7470bda4063f2a3dd05d16
Brandon L. Clark <support@themindspot.com>
2015-03-17 12:40:51 -0700

    something

--- index.js
+++ index.js
@@ -11,17 +11,17 @@ var D = require('./delete');
 
 var createAuthorization = function(Obj, cb){
 	C(Obj, function(err, user){
-	    if(err){return cb(err, null);}
+	    if(err){return cb(err, Obj);}
 	    
 	    if(user){			    	
 	    	// when new access is granted
     		R({credentials:{_id:user.id}},function(err, credentials){
-    			if(err){return cb(err, null);}
+    			if(err){return cb(err, Obj);}
     			
     			user.credentials = {};
     			user.credentials = credentials;
         		R({authorization:{user:user.id}},function(err, auths){
-        			if(err){return cb(err, null);}		
+        			if(err){return cb(err, Obj);}		
 
         			user.authorizations = [];
         			user.authorizations = auths;
@@ -29,7 +29,7 @@ var createAuthorization = function(Obj, cb){
 	        	});
         	});
 	    } else {
-    		return cb('!Error in creating.', null);
+    		return cb('!Error in creating.', Obj);
     	}	
 	});
 };
@@ -66,27 +66,27 @@ module.exports.create = function(Obj, cb){
 	if(Obj){
 		if(Obj.credentials){
 			R({service:{code:Obj.authorization.service}}, function(err, service){
-				if(err){return cb(err, null);}	
+				if(err){return cb(err, Obj);}	
 				
 				Obj.authorization.service = service.id;
 				R({access:{type: Obj.authorization.access.type, level:Obj.authorization.access.level}}, function(err, access){
-					if(err){return cb(err, null);}	
+					if(err){return cb(err, Obj);}	
 					
 					Obj.authorization.access = access.id;
 					C(Obj, function(err, user){
-					    if(err){return cb(err, null);}
+					    if(err){return cb(err, Obj);}
 
 					    if(user){
 				    		// When new credentials are created
 							R({authorization:{user:user.id}},function(err, auths){
-								if(err){return cb(err, null);}		
+								if(err){return cb(err, Obj);}		
 								
 								user.authorization = [];
 								user.authorization = auths;
 								return cb(null, user);
 							});    
 					    } else {
-				    		return cb('!Error in creating.', null);
+				    		return cb('!Error in creating.', Obj);
 				    	}
 		    		});
 		  		});    			
@@ -120,13 +120,14 @@ module.exports.create = function(Obj, cb){
 			});
     	} else if (Obj.social){ 
 			R({service:{code:Obj.social.service}}, function(err, service){
-				if(err){return cb(err, null);}	
+				if(err){return cb(err, Obj);}	
 				
 				Obj.social.service = service.id;
 				if(!Obj.id){
 					R({credentials:{email:Obj.email}},function(err, credentials){
 						if(err){return cb(err, Obj);}
 						
+						delete Obj.email;
 						Obj.id = credentials.id;
 						createAuthorization(Obj, function(err, user){
 							if(err){return cb(err, Obj);}
