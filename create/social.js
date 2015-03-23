3e54ea57cd1a87cebab2999863c1e0d109d80891
Brandon L. Clark <support@themindspot.com>
2015-03-23 08:43:43 -0700

    OBJECT STRUCTURE

--- create/social.js
+++ create/social.js
@@ -2,6 +2,7 @@ var Social = require('./../config').social;
 
 module.exports = function(Obj, cb){
     if(!Obj.user){ return cb('!No Object: User', null);}
+    if(!Obj.service){ return cb('!No Object: Service', null);}
 
 	var createObj = new Social(Obj);	
 	
--- index.js
+++ index.js
@@ -152,8 +152,8 @@ module.exports.create = function(Obj, cb){
 							
 							if(social){
 								var updateData = {};
-								updateData = U.social.getSearchFields(Obj);
-								updateData.service = service.id;
+								updateData.social = U.social.getSearchFields(Obj);
+								updateData.social = {service: service.id};
 								R({social:updateData}, function(err, socialService){
 									if(err){return cb(err, Obj);}
 									
